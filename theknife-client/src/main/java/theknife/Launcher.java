64c25ad2bf0b2ddcb9d0818185d1d61989ec12dd
Philipjj609 <philipciuca@gmail.com>
2026-05-21 23:50:32 +0200

    feat: Aggiungi Launcher per avvio proxy e aggiorna mainClass nel pom.xml

---
+++ theknife-client/src/main/java/theknife/Launcher.java
@@ -0,0 +1,11 @@
+package theknife;
+
+/**
+ * Launcher di avvio proxy per bypassare i controlli del runtime JavaFX 
+ * all'esecuzione del JAR shaded.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
